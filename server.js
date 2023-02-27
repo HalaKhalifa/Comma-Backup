4c364a081d1cf6138ca91ad4080d36c702f10cd0
Auxilium Technology <samer@auxiliumps.com>
2023-02-27 14:20:20 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+
+//-- Express configuration & Middleware
+app.set("view engine", "ejs"); // use EJS
+app.use(express.static(path.join(__dirname,'/public'))); // set path for assets folder
+
+app.use(express.json());
+//-----------------------------------
+
+//-- Express Router Configuration
+
+
+
+//-------------------------------
+
+
+//-- Server
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
