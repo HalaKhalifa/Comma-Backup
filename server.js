b7fa288973edb4098f15e44ba0d6b6363c8ebdfa
Auxilium Technology <samer@auxiliumps.com>
2023-05-04 00:06:47 +0300

    revert

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
+require("dotenv").config();
 const path = require("path");
+const mongoose=require("mongoose");
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const registrationRoutes = require("./routes/registrationRoutes");
+const LearnersRoutes = require("./routes/exampleRouter");
 
-require("dotenv").config();
+//-- Express configuration & Middleware
+app.set("view engine", "ejs"); // use EJS
+app.use(express.static(path.join(__dirname,'/public'))); // set path for assets folder
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.set("views", path.join(__dirname, "/views"));
+//-----------------------------------
 
-app.use("/signup", registrationRoutes);
+//-- Express Router Configuration
+app.use("/learners", LearnersRoutes);
 
-mongoose
-  .connect(process.env.MONGODB_CONN, { useNewUrlParser: true, useUnifiedTopology: true })
+
+//-------------------------------
+
+
+//-- Server
+
+mongoose.connect(process.env.MONGODB_CONN, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`Listening on port ${process.env.PORT}`);
