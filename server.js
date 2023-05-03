820fc97ebbf20ca59171ab7dbfca7538c75b4c36
Muhammad-Qzih <a90235145+1@gmail.com>
2023-05-03 23:37:30 +0300

    add ligin feature

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
-require("dotenv").config();
 const path = require("path");
-const mongoose=require("mongoose");
 const express = require("express");
 const app = express();
-const LearnersRoutes = require("./routes/exampleRouter");
-
+require("dotenv").config();
 //-- Express configuration & Middleware
+
 app.set("view engine", "ejs"); // use EJS
-app.use(express.static(path.join(__dirname,'/public'))); // set path for assets folder
+app.use(express.static(path.join(__dirname, "/public"))); // set path for assets folder
 
 app.use(express.json());
 //-----------------------------------
 
-//-- Express Router Configuration
-app.use("/learners", LearnersRoutes);
+app.use(express.urlencoded({ extended: false }));
+// --- Use cookie parser
 
+//----------------------------------------------------------------
+const session = require("express-session");
 
-//-------------------------------
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
+const mongoose = require("mongoose");
+const MONGO_URI =
+  "mongodb+srv://muhammad:muhammad@cluster0.pjbug9v.mongodb.net/user?retryWrites=true&w=majority";
 
-//-- Server
-
-mongoose.connect(process.env.MONGODB_CONN, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Listening on port ${process.env.PORT}`);
-    });
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
   });
+
+//----------------------------------------------------------------
+const authRoutes = require("./routes/authRoutes");
+const homeRoutes = require("./routes/homeRoutes");
+//-- Express Router Configuration
+app.use("/", authRoutes);
+app.use("/", homeRoutes);
+
+console.log(process.env.PORT);
+//-- Server
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
