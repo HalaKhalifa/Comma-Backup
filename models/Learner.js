a27d20d0ff9d290c3f7d4b6e320e47bde34b30da
Auxilium Technology <samer@auxiliumps.com>
2023-05-04 00:35:30 +0300

    --

--- models/Learner.js
+++ models/Learner.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
 
 const learnerSchema = new mongoose.Schema({
-     firstName: {
-        type: String,
-        required: true,
-      },
-      lastName: {
-        type: String,
-        required: true,
-      },
-      email:{
-        type:String,
-        required: true,
-      }
-},{timestamps:true});
+    firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-module.exports =  mongoose.model('Learner', learnerSchema)
+module.exports = mongoose.model('Learner', learnerSchema);
