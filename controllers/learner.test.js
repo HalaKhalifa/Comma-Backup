9624108af8f8845f446128b470abca4c85fc4bb6
iMohammadSawalha <me@mohammadsawalha.com>
2023-05-06 21:48:42 +0300

    added world map controller

--- controllers/learner.test.js
+++ controllers/learner.test.js
@@ -1,36 +1,54 @@
 const mongoose = require('mongoose')
 const learner = require('../models/learner.test')
 
+const getCountryLearners = async () => {
+  try {
+    const countryCounts = await learner.aggregate([
+      {
+        $group: {
+          _id: '$country',
+          count: { $sum: 1 }
+        }
+      }
+    ])
+
+    return countryCounts
+  } catch (error) {
+    console.error(error)
+    throw new Error('Failed to get country learners')
+  }
+}
+
 const getLearners = async () => {
-    try {
-        const learners = await learner.find();
-        return learners;
-    } catch (error) {
-        console.error("error : couldn't get Learners", error);
-        return null;
-    }
+  try {
+    const learners = await learner.find()
+    return learners
+  } catch (error) {
+    console.error("error : couldn't get Learners", error)
+    return null
+  }
 }
 const getOneLearner = async (req, res) => {
-    try {
-        const doc = await learner.findOne(req.body.condition).exec();
-        return doc;
-    } catch (error) {
-        console.error("error : couldn't get Learner", error);
-        return null;
-    }
+  try {
+    const doc = await learner.findOne(req.body.condition).exec()
+    return doc
+  } catch (error) {
+    console.error("error : couldn't get Learner", error)
+    return null
+  }
 }
 const updateLearner = async (req, res) => {
-    try {
-        var doc = await getOneLearner(req,res)
-        Object.keys(req.body.updatedata).forEach(key => {
-            doc[key] = req.body.updatedata[key];
-        })
-        await doc.save();
-        res.status(200).send()
-        return doc
-    } catch (error) {
-        console.error("error : couldn't update Learner", error);
-        res.status(500)
-    }
+  try {
+    var doc = await getOneLearner(req, res)
+    Object.keys(req.body.updatedata).forEach((key) => {
+      doc[key] = req.body.updatedata[key]
+    })
+    await doc.save()
+    res.status(200).send()
+    return doc
+  } catch (error) {
+    console.error("error : couldn't update Learner", error)
+    res.status(500)
+  }
 }
-module.exports = {getLearners,updateLearner,getOneLearner}
+module.exports = { getLearners, updateLearner, getOneLearner, getCountryLearners }
--- models/learner.test.js
+++ models/learner.test.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const learnerSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  active: { type: Boolean, required: true, default: false }
-});
+  country: { type: String, required: true }
+})
 
-const Learner = mongoose.model('Learner', learnerSchema);
+const Learner = mongoose.model('Learner', learnerSchema)
 
-module.exports = Learner;
+module.exports = Learner
