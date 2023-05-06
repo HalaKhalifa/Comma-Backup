c0ec807074b56854c67755ff78941f5a2a37e1af
Muhammad-Qzih <a90235145+1@gmail.com>
2023-05-06 22:46:08 +0300

    login frontend and improvement backend

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,47 +1,41 @@
-const User = require("../models/user");
+const User = require('../models/user')
 
-const {
-  set_session,
-  get_session_loggedIn,
-} = require("../middleware/sessionMiddleWare");
+const { set_session, get_session_loggedIn } = require('../middleware/sessionMiddleWare')
 
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt')
 
 const get_login = (req, res) => {
-  res.render("login", { title: "login" });
-};
+  res.render('login', { title: 'login', error: false })
+}
 
 const post_login = async (req, res) => {
   if (get_session_loggedIn(req)) {
-    return res.redirect("home");
+    return res.redirect('home')
   }
+  let email = req.body.email
+  console.log(username)
+  const user = await User.findOne({ email })
 
-  let username = req.body.username;
+  console.log('dfg')
+  console.log(user, 'form post')
+  console.log(user._id)
 
-  console.log(username);
+  const salt = await bcrypt.genSalt(10)
 
-  const user = await User.findOne({ username });
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
-  console.log("dfg");
-  console.log(user, "form post");
-  console.log(user._id);
+  const passwordsMatch = await bcrypt.compare(hashedPassword, user.password)
 
-  const salt = await bcrypt.genSalt(10);
+  if (user.email === email && passwordsMatch) {
+    await set_session(req, user._id)
 
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-  const passwordsMatch = await bcrypt.compare(hashedPassword, user.password);
-
-  if (user.username === username && passwordsMatch) {
-    await set_session(req, user._id);
-
-    res.redirect("home");
+    res.redirect('home')
   } else {
-    res.render("login", { title: "login" });
+    res.render('login', { title: 'login', error: true })
   }
-};
+}
 
 module.exports = {
   get_login,
-  post_login,
-};
+  post_login
+}
