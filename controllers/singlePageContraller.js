990e73be14312da302e9ef5c819f79113a7ec5a4
raghad zaidan <s12112557@stu.najah.edu>
2023-05-08 13:54:46 +0300

    edit css single page

--- controllers/singlePageContraller.js
+++ controllers/singlePageContraller.js
@@ -5,7 +5,6 @@ const getSingleCourse = async (req,res) => {
     const courseId = req.query.courseId;
    
     const singleCourse = await course.findById(courseId); 
-     console.log(singleCourse)
     if ( !singleCourse ) {
         return res.status(404).json({error: 'No such course'});
     }
