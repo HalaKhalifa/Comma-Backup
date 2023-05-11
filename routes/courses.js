3fb56c7eceef8c8626db536a79d1374c46e09952
FarahSaleh121 <s12112132@stu.najah.edu>
2023-05-11 22:06:50 +0300

    sorting in courses page

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const router = express.Router()
-const { coursePagination, getSingleCourse, searchCourses, } = require('../controllers/courses')
-const formController=require('../controllers/courses')
-
+const { coursePagination, getSingleCourse, searchCourses,getSortedCourses } = require('../controllers/courses')
+const formController =require('../controllers/courses')
+const deleteCourse = require('../controllers/courses')
+ 
 router.post("/dashboard/courses",formController.createNewCourse);
-router.get('/courses', coursePagination)
+router.get('/courses', async (req, res) => {
+    try {
+      const pageCourses = await coursePagination(req, res);
+      res.render('pages/home/courses_page.ejs', {
+          title: 'courses page',
+          pageCourses: pageCourses
+        })
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 router.get('/outline', getSingleCourse)
+router.post("/dashboard/courses",deleteCourse.updateCourse)
+router.post('/courses',getSortedCourses)
 
 // router.post('/search', async (req, res) => {
 //   const searchQuery = req.body.searchText
